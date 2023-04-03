f87e8e00ef728ee6ac179efa38c95fbfc4da4a31
DilanJT <dicmandilan@gmail.com>
2023-04-03 11:26:10 +0530

    added the different components STARTER

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/Header/header';
+
+const Home = () => {
+  return (
+    <div className="container my-5">
+      <h1>Welcome to the Code Translator Tool</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec ante
+        vel dui sagittis elementum eu a nulla. Sed eleifend mauris sit amet
+        mauris accumsan volutpat.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  );
+};
+
+export default Home;
