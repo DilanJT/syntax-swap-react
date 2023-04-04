452278db999d10d58a46188ad47d0709163a7794
DilanJT <dicmandilan@gmail.com>
2023-04-04 09:47:19 +0530

    added the translator component for translator page

--- src/components/TranslatorTab/translatortab.jsx
+++ src/components/TranslatorTab/translatortab.jsx
@@ -1,46 +1,60 @@
 import React, { useState } from 'react';
 
 const CodeTranslatorTab = () => {
-  const [javaCode, setJavaCode] = useState('');
-  const [jsCode, setJsCode] = useState('');
+    const [javaCode, setJavaCode] = useState('');
+    const [jsCode, setJsCode] = useState('');
 
-  const handleTranslate = () => {
-    // TODO: implement code translation logic
-    setJsCode('Translated JavaScript code goes here');
-  };
+    const handleTranslate = () => {
+        // TODO: implement code translation logic
+        setJsCode('Translated JavaScript code goes here');
+    };
 
-  return (
-    <div className="container my-5">
-      <h1>Code Translator</h1>
-      <div className="row mt-3">
-        <div className="col-12 col-md-6">
-          <h3>Java Code</h3>
-          <textarea
-            className="form-control"
-            rows="10"
-            value={javaCode}
-            onChange={(e) => setJavaCode(e.target.value)}
-          ></textarea>
+    const handleCompile = () => {
+        // TODO: implement code compilation logic
+        alert("compilation triggered")
+    }
+
+    return (
+        <div className="container my-5">
+            <h1>Translate Java to JavaScript</h1>
+            <div className="row mt-3">
+                <div className="col-12 col-md-6">
+                    <h3>Java Code</h3>
+                    <textarea
+                        className="form-control"
+                        rows="10"
+                        value={javaCode}
+                        onChange={(e) => setJavaCode(e.target.value)}
+                    ></textarea>
+                    <div className="row mt-3">
+                        <div className="col-12">
+                            <button className="btn btn-primary" onClick={handleCompile}>
+                                Compile
+                            </button>
+
+                        </div>
+
+                        <div className="col-12 mt-3">
+                            <button className="btn btn-primary" onClick={handleTranslate}>
+                                Translate
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="col-12 col-md-6">
+                    <h3>Translated JavaScript Code</h3>
+                    <textarea
+                        className="form-control"
+                        rows="10"
+                        value={jsCode}
+                        readOnly
+                    ></textarea>
+                </div>
+            </div>
+
         </div>
-        <div className="col-12 col-md-6">
-          <h3>Translated JavaScript Code</h3>
-          <textarea
-            className="form-control"
-            rows="10"
-            value={jsCode}
-            readOnly
-          ></textarea>
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-12">
-          <button className="btn btn-primary" onClick={handleTranslate}>
-            Translate and Compile
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CodeTranslatorTab;
--- src/pages/Translator/translator.jsx
+++ src/pages/Translator/translator.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
+import CodeTranslatorTab from '../../components/TranslatorTab/translatortab';
 
 const Translator = () => {
   return (
     <div className="container my-5">
-      <h1>Welcome to the SyntaxSwap Code Translator Tool</h1>
-      <p>
-       Translator page
-      </p>
-      <button className="btn btn-primary">Get Started</button>
+      
+      <CodeTranslatorTab />
     </div>
   );
 };
